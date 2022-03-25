42ee0afa153a67c0f70826f1e279effc7a3d1756
Marcin Rudzki <mar.rudzki@gmail.com>
2022-03-25 08:02:19 +0100

    first getUserByUsername implementation

---
+++ src/db.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+export const getUserByUsername = async username => {
+    const client = await MongoClient.connect(
+        'mongodb://localhost:27017/TEST_DB', {
+            useNewUrlParser: true
+        }
+    );
+
+    const db = client.db('TEST_DB');
+
+    const result = await db.collection('users').findOne({ username });
+
+    client.close();
+
+    return result;
+}
