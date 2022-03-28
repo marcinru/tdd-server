9d13a9a6cf1b49916cd27181e8794c745fc42f88
Marcin Rudzki <mar.rudzki@gmail.com>
2022-03-28 07:45:15 +0200

    add test for 404 not found

--- src/server.js
+++ src/server.js
@@ -7,7 +7,11 @@ app.get('/users/:username', async (req, res) => {
     const { username } = req.params;
     try {
         const user = await db.getUserByUsername(username);
-        res.json(user);
+        if (!user) {
+            res.status(404).send();
+        } else {
+            res.json(user);
+        }
     } catch (error) {
         res.status(500).json(error);
     }
