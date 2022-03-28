1cd5a5700c72b3c150cc5356d5df661322368f6d
Marcin Rudzki <mar.rudzki@gmail.com>
2022-03-28 07:13:57 +0200

    add db test for user not found

--- src/db.test.js
+++ src/db.test.js
@@ -1,23 +1,24 @@
 import { expect } from 'chai';
+import { async } from 'regenerator-runtime';
 import { getUserByUsername } from './db';
 import { getDatabaseData, setDatabaseData, resetDatabase } from './test-helpers';
 
 describe('getUserByUsername', () => {
+    const fakeData = [{
+        id: '123',
+        username: 'abc',
+        email: 'abc@gmail.com'
+    }, {
+        id: '124',
+        username: 'wrong',
+        email: 'wrong@gmail.com'
+    }];
+
     afterEach('reset the database', async () => {
         await resetDatabase();
     });
 
     it('gets the correct user from the database given a username', async () => {
-        const fakeData = [{
-            id: '123',
-            username: 'abc',
-            email: 'abc@gmail.com'
-        }, {
-            id: '124',
-            username: 'wrong',
-            email: 'wrong@gmail.com'
-        }];
-
         await setDatabaseData('users', fakeData);
 
         const actual = await getUserByUsername('abc');
@@ -31,5 +32,13 @@ describe('getUserByUsername', () => {
 
         expect(actual).excludingEvery('_id').to.deep.equal(expected);
         expect(finalDBState).excludingEvery('_id').to.deep.equal(fakeData);
+    });
+
+    it('returns null when the user is not found', async () => {
+        await setDatabaseData('users', fakeData);
+
+        const actual = await getUserByUsername('notExistingOne');
+
+        expect(actual).to.be.null;
     });
 });
